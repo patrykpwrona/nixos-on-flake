325c6ae1349e83b2709368e278f169ca19a615f4
Patryk Wrona <patryk.wrona92@gmail.com>
2024-09-20 10:59:06 +0200

    packages

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ DISCLAIMER: It's not my live configuration repo, but I put here things which can
 
 ## Why flake?
 * I can install one package permanently without updating whole system.
-  * Maybe its worse for security, but gives more control over system. E.g. on Ubuntu I can `apt install` one thing
+  * Maybe its worse for security, but gives more control over system. E.g. on Ubuntu I can `apt install` one thing without updating all packages.
   * `apt update` = `nix flake update .`
   * `apt upgrade` = `sudo nixos-rebuild switch --flake ./#dell`
   * `apt install x` = add x to definition in nix file and `sudo nixos-rebuild switch --flake ./#dell`
@@ -14,9 +14,14 @@ DISCLAIMER: It's not my live configuration repo, but I put here things which can
 ## Why home-manager?
 * Actually, there is no point of it in my use case.
   * I have a plan to get rid of it and move everything into NixOS configuration.
-* It make sense if you want to share home config with other operating systems (macOS, other GNU/Linux distributions)
+* It make sense if you want to share home config with other operating systems, where home-manager can be installed (macOS, other GNU/Linux distributions).
 
 ## What it can?
 * Both NixOS and home-manager config in a single flake.
-* Install specific packages from unstable channel.
+  * [Done here](./flake.nix)
+* Install all packages from stable but some from unstable channel.
+  * [Declare both stable and unstable as input](./flake.nix#L5-L6)
+  * [Pass them to flake](./flake.nix#L13-L24)
+  * [Read them as a paremeter](./common/packages.nix#L1)
+  * [Install from unstable](./common/packages.nix#L60)
 * Various fixes which I found on the way.
---
+++ common/packages.nix
@@ -0,0 +1,119 @@
+{ config, pkgs, pkgs-unstable, lib, ... }:
+{
+  ## Do not install default gnome apps
+  services.gnome.core-utilities.enable = false;
+
+  ## Packages installed as user
+  users.users.pw.packages = with pkgs; [
+    ### GNOME apps
+    # On ustable channel delete 'gnome.' prefix
+    gnome.gnome-terminal # another terminal app as a backup
+    gnome.gnome-keyring # for ssh
+    gnome.gnome-tweaks # for setting Caps Lock as additional Ctrl (Tweaks -> Keyboard -> Additional layout Options -> Caps Lock behavior)
+    gnome.nautilus # file manager
+    gnome.eog # image viewer
+
+    ### GNOME extensions
+    # To enable gnome extensions: log out and login, open "Extension" app and enable
+    gnomeExtensions.appindicator # needed for tray icons to work (e.g for nextcloud)     
+    gnomeExtensions.tophat # system usage status
+    libgtop # for tophat
+    gnomeExtensions.vitals # system usage status
+    gnomeExtensions.hibernate-status-button # hibernate button
+
+    ### Python modules and apps
+    (python311.withPackages(py-packages: with py-packages; [ pyftpdlib ]))
+    # python3 -m pyftpdlib
+
+    ### Vscode with extensions - moved to home-manager now, but this is working
+    #(vscode-with-extensions.override {
+    #  vscodeExtensions = with vscode-extensions; [
+    #    pkief.material-icon-theme        
+    #    eamodio.gitlens
+    #    bbenoist.nix
+    #  ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    #    {
+    #      name = "git-last-commit-message";
+    #      publisher = "JanBn";
+    #      version = "1.9.0";
+    #      sha256 = "atINTAGN0RVYMiiz9LZyPt1YU1IIdlqCjptQo/SpoPM="; # set to anything, first rebuild fail tell you the correct one
+    #    }
+    #  ];
+    #})
+
+    ### Other apps
+    papers # pdf viewer
+    brave # browser with good adblock
+    keepassxc # password manager
+    nextcloud-client # nextcloud
+    bc # console calculator
+    onlyoffice-bin # office
+    gnome-frog # screenshot OCR - works but not so good - need to manually open screenshot from files     
+    blanket # ambient sounds to focus
+    gnome-podcasts # podcasts
+    vlc # media player
+    alass # subtitle synchronization - alass-cli (I cannot get ffsubsync to work)
+    ffmpeg_7 # for alass
+    gramps # family tree
+    graphviz # for dot for gramps
+    
+    pkgs-unstable.immich-cli # immich cli - version from stable is very old, and not working         
+
+  ];
+
+  ## Packages installed as root (system-wide)
+  environment.systemPackages = with pkgs; [
+    home-manager
+    tree
+    vim
+    wget
+    rsync
+    git
+    killall
+    file
+    du-dust # dust - du alternative
+    ripgrep # rg - grep alternative
+    fd # find alternative
+    tldr
+    unzip
+    jq
+    fping
+    htop
+    dig
+    pwgen
+    lm_sensors
+    testdisk
+    smartmontools
+    ethtool
+    nvme-cli # nvme
+    gptfdisk # gdisk
+    dosfstools # fsck.vfat
+    parted # partprobe
+    fatrace # show processes which do something on disk
+  ];
+
+  ## Programs
+  programs.mtr.enable = true;
+
+  ## Flatpak
+  # flatpak install net.xmind.XMindG # mind map tool
+  services.flatpak.enable = true;
+
+  ## Packages added to Gnome session search path
+  ### Fixes for various packages
+  services.xserver.desktopManager.gnome.sessionPath = [
+    pkgs.libgtop # for tophat
+    pkgs.goocanvas3 # for gramps
+    pkgs.goocanvas2 # for gramps
+    pkgs.gobject-introspection # for gramps
+  ];
+  
+  ## Fix for:
+  ## Your GStreamer installation is missing a plug-in.
+  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
+  gst-plugins-good
+  gst-plugins-bad
+  gst-plugins-ugly
+  gst-libav
+]);
+}
